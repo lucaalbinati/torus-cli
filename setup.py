e444b40c204624a40852032b039826bb86985e7a
Luca Albinati <luca.albinati@gmail.com>
2021-11-05 13:55:54 +0100

    patch release v0.9.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="torus-cli",
-    version="0.1.3",
+    version="0.9.1",
     author="Luca Albinati",
     author_email="luca.albinati@gmail.com",
     description="Command line interface for torus-engine",
